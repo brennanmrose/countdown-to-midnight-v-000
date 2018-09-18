daa2c9c89e7478d526eb67aed9911b803cb8a214
Rose Brennan <brennan.m.rose@gmail.com>
2018-09-18 15:08:42 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,9 +2,16 @@ def countdown(number = 10)
   while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
-    sleep(1)
+    #sleep(1)
   end  
     "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
+def countdown_with_sleep(number = 10)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep(1)
+  end  
+    "HAPPY NEW YEAR!"
+end
